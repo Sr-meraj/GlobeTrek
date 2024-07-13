ab2f495842bab28f1616169d25b8731788537968
Sr-meraj <marajmia3232@gmail.com>
2024-07-13 14:22:31 +0600

    Language dropdown implementation

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -6,3 +6,23 @@ document.querySelector('.navbar-toggler').onclick = function() {
     div.classList.add('show')
   }
 };
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdownToggle = document.getElementById('navbarDropdown');
+  const dropdownMenu = dropdownToggle.nextElementSibling; // Select the <ul> element
+
+  dropdownToggle.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    
+    // Toggle the 'show' class on the dropdown menu
+    dropdownMenu.classList.toggle('show');
+  });
+
+  // Close the dropdown if clicked outside
+  document.addEventListener('click', function (event) {
+    if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+      dropdownMenu.classList.remove('show');
+    }
+  });
+});
